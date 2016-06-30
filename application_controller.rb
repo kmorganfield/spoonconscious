0eb028630d0d6f113bb373a2588d82a6014337cb
Isabella Moak <isabellamartinem@gmail.com>
2016-06-30 17:28:26 +0000

    The ruby is working wth the checkbox.erb and recipe_page.erb

--- application_controller.rb
+++ application_controller.rb
@@ -1,8 +1,20 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/recipes.rb'
 
 class MyApp < Sinatra::Base
 
+  get '/recipes' do
+    erb :checkbox
+  end
 
+  post '/new_recipes' do
+    allergy=params[:option].to_sym
+    r = put_recipes(allergy)
+    @link = r[:banana_icecream][:link]
+    @link2 = r[:cake][:link]
+    @link3 = r[:salad][:link]
+    erb :recipe_page
+  end
 
 end
