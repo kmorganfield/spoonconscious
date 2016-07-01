c2cb2bba11c4122923f4e1519a510bb901486452
London Bielicke <eileenandlondongaming@gmail.com>
2016-07-01 15:03:15 +0000

    photos

--- application_controller.rb
+++ application_controller.rb
@@ -25,6 +25,16 @@ class MyApp < Sinatra::Base
       @title7 = r[:peanut_cookies][:title]
       @title8 = r[:vg_fajita][:title]
       @title9 = r[:vg_cake][:title]
+
+      @content = r[:cinnamon_rolls][:content]
+      @content2 = r[:smoothie][:content]
+      @content3 = r[:rigatoni][:content]
+      @content4 = r[:vegan_chili][:content]
+      @content5 = r[:buhda_bowl][:content]
+      @content6 = r[:spring_rolls][:content]
+      @content7 = r[:peanut_cookies][:content]
+      @content8 = r[:vg_fajita][:content]
+      @content9 = r[:vg_cake][:content]
     elsif allergy.to_s == "vegetarian"
       @title = r[:granola][:title]
       @title2 = r[:panini][:title]
@@ -35,6 +45,16 @@ class MyApp < Sinatra::Base
       @title7 = r[:popsicles][:title]
       @title8 = r[:v_fajita][:title]
       @title9 = r[:chips][:title]
+
+      @content = r[:granola][:content]
+      @content2 = r[:panini][:content]
+      @content3 = r[:rice][:content]
+      @content4 = r[:vegetarian_chili][:content]
+      @content5 = r[:buhda_bowl][:content]
+      @content6 = r[:veggie_pizza][:content]
+      @content7 = r[:popsicles][:content]
+      @content8 = r[:v_fajita][:content]
+      @content9 = r[:chips][:content]
     elsif allergy.to_s == "gluten_free"
       @title = r[:salad][:title]
       @title2 = r[:chicken][:title]
@@ -45,6 +65,16 @@ class MyApp < Sinatra::Base
       @title7 = r[:yogurt_popsicles][:title]
       @title8 = r[:gf_fajita][:title]
       @title9 = r[:gf_cake][:title]
+
+      @content = r[:salad][:content]
+      @content2 = r[:chicken][:content]
+      @content3 = r[:cupcakes][:content]
+      @content4 = r[:glutenfree_chili][:content]
+      @content5 = r[:buhda_bowl][:content]
+      @content6 = r[:cauliflower_pizza][:content]
+      @content7 = r[:yogurt_popsicles][:content]
+      @content8 = r[:gf_fajita][:content]
+      @content9 = r[:gf_cake][:content]
     else
       return "nothing"
     end
